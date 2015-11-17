358232679ffaefe7746d37e6bcf27ff32bd306b7
Todd Crone <todd.crone@perceptivesoftware.com>
2015-11-17 13:38:49 -0500

    Add first Jasmine Spec

---
+++ controllerSpec.js
@@ -0,0 +1,22 @@
+describe('Controller: ListCtrl', function() {
+
+  beforeEach(module('notesApp'));
+
+  var ctrl;
+
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('ListCtrl');
+  }));
+
+  it('should have items available on load', function() {
+    expect(ctrl.items).toEqual([
+      {id: 1, label: 'First', done: true},
+      {id: 2, label: 'Second', done: false}
+    ]);
+  });
+
+  it('should have highlight items based on state', function() {
+    var item = {id: 1, label: 'First', done: true};
+    
+  });
+});
