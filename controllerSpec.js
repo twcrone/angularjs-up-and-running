035566092ecdbc46a69285975853aed671a64825
Todd Crone <todd.crone@perceptivesoftware.com>
2015-11-17 13:49:14 -0500

    Add last part of tests for Jasmine

--- controllerSpec.js
+++ controllerSpec.js
@@ -18,5 +18,15 @@ describe('Controller: ListCtrl', function() {
   it('should have highlight items based on state', function() {
     var item = {id: 1, label: 'First', done: true};
     
+    var actualClass = ctrl.getDoneClass(item);
+
+    expect(actualClass.finished).toBeTruthy();
+    expect(actualClass.unfinished).toBeFalsy();
+
+    item.done = false;
+    actualClass = ctrl.getDoneClass(item);
+
+    expect(actualClass.finished).toBeFalsy();
+    expect(actualClass.unfinished).toBeTruthy();
   });
 });
